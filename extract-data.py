6446ed9e3f473789513544bd1b09a26b9a3b34bf
Lindah Nyambura <l.nyambura@alustudent.com>
2023-10-04 20:17:18 +0200

    few edits and rgb pattern

--- extract-data.py
+++ extract-data.py
@@ -3,60 +3,60 @@ import re
 #our sample data
 data = """
 Restaurant and Cuisine:
-Deli Delights - Sandwiches"
-Taste of Italy - Italian"
-Burger Blitz - Fast Food"
-Vegan Vibes - Vegan"
-Sushi Central - Japanese"
+Deli Delights - Sandwiches
+Taste of Italy - Italian
+Burger Blitz - Fast Food
+Vegan Vibes - Vegan
+Sushi Central - Japanese
 
 Ingredient Lists:
-Flour, Sugar, Eggs, Butter"
-Tomatoes, Basil, Mozzarella, Olive Oil"
-Chicken, Lettuce, Mayo, Bread"
-Rice, Salmon, Avocado, Seaweed"
+Flour, Sugar, Eggs, Butter
+Tomatoes, Basil, Mozzarella, Olive Oil
+Chicken, Lettuce, Mayo, Bread
+Rice, Salmon, Avocado, Seaweed
 
 RGB Colors:
-rgb(255, 0, 0)"
-rgb(0, 255, 0)"
-rgb(0, 0, 255)"
-rgb(128, 128, 128)"
+rgb(255, 0, 0)
+rgb(0, 255, 0)
+rgb(0, 0, 255)
+rgb(128, 128, 128)
    
 Social Media Usernames:
-@johnDoe45"
-@tech_guru"
-@codingMaster23"
-@foodieLover"
+@johnDoe45
+@tech_guru
+@codingMaster23
+@foodieLover
 
 Product Codes:
-XYZ001"
-ABC789"
-MNO123"
-PQR456"
+XYZ001
+ABC789
+MNO123
+PQR456
    
 News Headlines:
-Economy on the Rise: Stock Market Hits New Highs"
-New Species Discovered: Scientists Excited About Findings"
-Sports Update: Local Team Takes the Championship!"
-Weather Warning: Heavy Rain Predicted for the Weekend"
+Economy on the Rise: Stock Market Hits New Highs
+New Species Discovered: Scientists Excited About Findings
+Sports Update: Local Team Takes the Championship!
+Weather Warning: Heavy Rain Predicted for the Weekend
    
 Event Dates/Times:
-Jan 12, 2023 - 09:00 AM"
-Feb 28, 2023 - 03:00 PM"
-Dec 25, 2023 - 12:00 PM"
-Oct 31, 2023 - 06:00 PM"
+Jan 12, 2023 - 09:00 AM
+Feb 28, 2023 - 03:00 PM
+Dec 25, 2023 - 12:00 PM
+Oct 31, 2023 - 06:00 PM
    
 Email Addresses:
-johndoe@email.com"
-tech.support@webserver.net"
-sales@mybusiness.org"
-info@school.edu"
+johndoe@email.com
+tech.support@webserver.net
+sales@mybusiness.org
+info@school.edu
 """
 
 # defining our regex patterns
 
 restaurant_pattern = re.compile(r'^([^\d-]+) -([^\d-]+)$', re.MULTILINE)
 ingredient_pattern = re.compile(r'')
-rgb_pattern = re.compile(r'')
+rgb_pattern = re.compile(r'rgb\((\d+), (\d+), (\d+)\)', re.MULTILINE')
 usernames_pattern = re.compile(r'')
 productcode_pattern = re.compile(r'')
 headlines_pattern = re.compile(r'')
