8b46c58bf245db8ff2807c35785bb2d826c4bada
Lindah Nyambura <127734490+lindahnyambura@users.noreply.github.com>
2023-10-04 23:10:08 +0200

    Update extract-data.py

--- extract-data.py
+++ extract-data.py
@@ -57,8 +57,8 @@ delightsrestaurant@yahoo.fr
 
 restaurant_pattern = re.compile(r'^([^\d-]+) -([^\d-]+)$', re.MULTILINE)
 ingredient_pattern = re.compile(r'^([^\d-]+(?:, ([^\d-]+))*)$', re.MULTILINE)
-rgb_pattern = re.compile(r'^rgb\((\d+), (\d+), (\d+)\)$', re.MULTILINE)
-usernames_pattern = re.compile(r'^@(\w+)$', re.MULTILINE)
+rgb_pattern = re.compile(r'rgb\((\d+), (\d+), (\d+)\)', re.MULTILINE)
+usernames_pattern = re.compile(r'@(\w+)', re.MULTILINE)
 productcode_pattern = re.compile(r'[A-Z]{3}[0-9]{3}', re.MULTILINE)
 headlines_pattern = re.compile(r'([^\d-]+): ([^\d-]+)$', re.MULTILINE)
 dates_pattern = re.compile(r'([A-Za-z]{3} \d{2}, \d{4} - \d{2}:\d{2} (AM|PM))', re.MULTILINE)
